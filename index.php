e6614be90488cc1be2948a3c7423c5bd575ea94b
eline29 <eline.thieulin@etu.uca.fr>
2026-01-19 11:00:16 +0100

    final

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-<?php 
+    <?php 
   include('header et footer/head.php'); 
 ?>
 <body>
@@ -20,26 +20,44 @@
         </section>
 
         <section class="section-carousel">
-            <div class="container carousel-layout">
-                <div id="main-carousel" class="carousel-container">
-                    <button class="carousel-btn prev" aria-label="Précédent">❮</button>
-                    <button class="carousel-btn next" aria-label="Suivant">❯</button>
-                    <div class="carousel-track">
-                        <div class="carousel-item"><img src="images/attrape poil.webp" alt=""></div>
-                        <div class="carousel-item"><img src="images/Logo-1.webp" alt=""></div>
-                    </div>
-                </div>
-            </div>
-        </section>
+    <div class="container carousel-layout">
+        
+        <div id="main-carousel" class="carousel-container">
+            <button class="carousel-btn prev" aria-label="Précédent">❮</button>
+            <button class="carousel-btn next" aria-label="Suivant">❯</button>
 
+            <div class="carousel-track">
+                <!-- Mettre les bonnes images-->
+                <div class="carousel-item"><img src="images/attrape poil.webp" alt=""></div>
+                <div class="carousel-item"><img src="images/decapsuleur.webp" alt=""></div>
+                <div class="carousel-item"><img src="images/banniere.webp" alt=""></div>
+            </div>
+            
+            <div class="carousel-nav"></div>
+        </div>
+
+        <div class="carousel-text">
+            <h2>Leur futur commence par votre visite : découvrez-les tous.</h2>
+        </div>
+        
+    </div>
+</section>
         <section class="section-about">
             <div class="container">
-                <h3>À Propos de nous</h3>
-                <p>La SPA de la Haute-Loire accueille toute l'année une soixantaine de chiens et une trentaine de chats et chatons venant des communes conventionnées.</p>
+                <h3>A Propos de nous</h3>
+                <p>La SPA a vu le jour en 1983 grâce à une poignée d'amoureux des animaux. Pendant près de 10 ans, ses
+                    locaux se situèrent au rez-de-chaussée d'une maison vétuste Rue des Farges au Puy en Velay. Lieu peu
+                    approprié pour recevoir les chiens ou chats en errance. Le travail des bénévoles y était difficile
+                    et il n'y avait alors que très peu d'organisation. Enfin en 1994, le refuge actuel est "sorti" de
+                    terre grâce à l'acharnement de ces mêmes bénévoles et de quelques élus convaincus de la nécessité de
+                    ce site. Aujourd'hui, la SPA de la Haute-Loire est affiliée à la Confédération Nationale Défense de
+                    l'Animal. Elle accueille toute l'année une soixantaine de chiens et une trentaine de chats et
+                    chatons venant des communes conventionnées.</p>
             </div>
         </section>
 
-        <section id="contact" class="section-contact">
+
+           <section id="contact" class="section-contact">
             <div class="container">
                 <div class="contact-flex-container">
                     <div class="contact-left-column">
@@ -53,30 +71,37 @@
                     <div id="mapSPA_XX" class="map-box"></div>
                 </div>
 
-                <div class="contact-form">
+            <div class="contact-form">
                 <h3>Formulaire</h3>
                 <form>
                     <div class="form-row">
                         <div class="input-group">
-                                    <label>Nom</label>
-                                    <input type="text" required>
-                                </div>
-                                <div class="input-group">
-                                    <label>E-mail</label>
-                                    <input type="email" required>
-                                </div>
-                            </div>
-                            <div class="input-group">
-                                <label>Message</label>
-                                <textarea rows="5" placeholder="Comment pouvons-nous vous aider ?"></textarea>
-                            </div>
-                            <button type="submit" class="btn-submit">Envoyer</button>
-                        </form>
+                            <label>Nom</label>
+                            <input type="text">
+                        </div>
+                        <div class="input-group">
+                            <label>E-mail</label>
+                            <input type="email">
+                        </div>
                     </div>
-                </div>
+                    <div class="input-group">
+                        <label>Téléphone</label>
+                        <input type="tel">
+                    </div>
+                    <div class="input-group">
+                        <label>Objet</label>
+                        <input type="text">
+                    </div>
+                    <div class="input-group">
+                        <label>Message</label>
+                        <textarea rows="4"></textarea>
+                    </div>
+                    <button type="submit" class="btn-submit">Envoyer</button>
+                </form>
+            </div>
+
             </div>
         </section>
-
         <button id="scrollToTop" class="scroll-to-top">↑</button>
     </main>
 
@@ -84,4 +109,5 @@
   include('header et footer/footer.php'); 
 ?>
 </body>
+
 </html>
